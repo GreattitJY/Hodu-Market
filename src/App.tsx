1f406b6b096e19820a63274c044b932b39a3cdcd
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-24 10:29:24 +0900

    setting: 스타일, 테마, 라우터 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import AppRouter from "./pages/Router";
+import { GlobalStyle } from "./style/GolbalStyle";
+import { theme } from "./style/theme";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider {...{ theme }}>
+        <AppRouter />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
