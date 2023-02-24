09461e28275ed202cc0010d2a0f288c01a153312
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-24 18:08:51 +0900

    style: 글로벌 스타일 추가(button, input, sr-out)

--- src/style/GolbalStyle.tsx
+++ src/style/GolbalStyle.tsx
@@ -5,7 +5,33 @@ import reset from "styled-reset";
 export const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  :root {
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    font-size: 10px
+  }
+
   * {
     box-sizing: border-box;
+  }
+
+  input,
+  button {
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    font: inherit;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+
+  .sr-out {
+    overflow: hidden;
+    position: absolute;
+    width: 0;
+    height: 0;
+    line-height: 0;
+    text-indent: 0;
   }
 `;
