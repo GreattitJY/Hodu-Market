c37008b9e74e4d1d24ebf8e4041f6fa4af207a56
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-25 11:22:43 +0900

    add: 404, Home, Router 페이지 추가 (라우터 연결만)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Header />
+      <main>메인</main>
+      <Footer />
+    </>
+  );
 }
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from "./404";
 import HomePage from "./Home";
+import LoginPage from "./Login";
 
 export default function AppRouter() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route index element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
