471cdc8aa3e88607bbbbb24fdf4ac3d8708457bf
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-26 15:49:52 +0900

    style: 헤더 컴포넌트 마크업 및 스타일 추가

--- src/components/Header/HeaderStyle.ts
+++ src/components/Header/HeaderStyle.ts
@@ -1,19 +1,15 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
-  display: flex;
-  justify-content: center;
+  padding: 22px;
+  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.1);
 `;
 
 export const HeaderContainer = styled.div`
-  max-width: ${({ theme }) => theme.basicWidth};
-  /* max-width: calc(100vw - 30%); */
   display: flex;
-  flex: 1;
-  /* justify-content: space-between; */
   align-items: center;
-  box-shadow: inset 0 0 10px blue;
-  padding: 22px 0;
+  max-width: ${({ theme }) => theme.basicWidth};
+  margin: 0 auto;
 `;
 
 export const LogoImg = styled.img`
@@ -23,14 +19,19 @@ export const LogoImg = styled.img`
 
 export const SearchForm = styled.form`
   position: relative;
+  margin-left: 30px;
+  margin-right: auto;
 
   input {
     width: 400px;
     border: 2px solid ${({ theme }) => theme.color.green100};
     border-radius: 50px;
     padding: 13px 50px 13px 13px;
-    margin-left: 30px;
     font-size: 1.6rem;
+
+    @media screen and (max-width: 700px) {
+      max-width: 200px;
+    }
   }
 
   button {
@@ -42,5 +43,26 @@ export const SearchForm = styled.form`
     width: 28px;
     height: 28px;
     text-indent: -9999px;
+  }
+`;
+
+export const NavContainer = styled.nav`
+  display: flex;
+  gap: 26px;
+  flex-shrink: 0;
+
+  a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 4px;
+    color: ${({ theme }) => theme.color.gray100};
+    font-size: 12px;
+    font-weight: 400;
+
+    img {
+      width: 28px;
+      height: 28px;
+    }
   }
 `;
