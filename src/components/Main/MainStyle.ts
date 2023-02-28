1b819ab64d41ed3c883444d8f3e46ff9abccffa1
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-28 11:42:18 +0900

    style: main container 레이아웃 추가

---
+++ src/components/Main/MainStyle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  max-width: ${({ theme }) => theme.basicWidth};
+  margin: 80px auto 180px;
+`;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import GetProduct from "./GetProduct";
+import { MainContainer } from "./MainStyle";
 
 export default function Main() {
   return (
     <main>
-      <GetProduct />
+      <MainContainer>
+        <GetProduct />
+      </MainContainer>
     </main>
   );
 }
