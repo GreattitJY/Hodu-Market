e199141fc7b0143a1034425e80e2fd67d9711ce1
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-01 10:03:19 +0900

    stlye: 메인 레이아웃 변경(flex)

--- src/components/Main/MainStyle.ts
+++ src/components/Main/MainStyle.ts
@@ -2,5 +2,9 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   max-width: ${({ theme }) => theme.basicWidth};
-  margin: 80px auto 180px;
+  margin: 80px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 80px;
 `;
