19056215386230a201fa5a2f1a85dcaf5222c671
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-01 10:02:33 +0900

    add: PageNationBtnProps interface 추가(현재 페이지 정보 전달)

--- src/export/interface/index.tsx
+++ src/export/interface/index.tsx
@@ -1,8 +1,12 @@
 export interface PageNationProps {
   maxPage: number;
+  productsPage: number;
   setProductsPage: React.Dispatch<React.SetStateAction<number>>;
   data: {
     previous: number | null;
     next: number | null;
   };
+}
+export interface PageNationBtnProps {
+  productsPage: number;
 }
