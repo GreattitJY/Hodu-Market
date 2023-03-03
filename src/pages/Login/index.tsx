03e11487775b2882c7cdfe4e32e16615ced477c0
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-04 07:23:33 +0900

    style: login 페이지 마크업 진행중

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <img src="" alt="" />
+      <article></article>
+      <nav>
+        <Link to="/">회원가입</Link>
+        <Link to="/">비밀먼호 찾기</Link>
+      </nav>
+    </div>
+  );
 }
