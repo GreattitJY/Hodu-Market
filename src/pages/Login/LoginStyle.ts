ad3815d530a18f16b359976d595fffa14aa9ea97
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-05 18:53:09 +0900

    style: login 페이지 스타ㅣ일 작업중

---
+++ src/pages/Login/LoginStyle.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const LoginWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LoginContainer, LoginWrap } from "./LoginStyle";
 
 export default function LoginPage() {
+  const logoImg = "/assets/images/Logo-hodu.png";
+
   return (
-    <div>
-      <img src="" alt="" />
-      <article></article>
-      <nav>
-        <Link to="/">회원가입</Link>
-        <Link to="/">비밀먼호 찾기</Link>
-      </nav>
-    </div>
+    <LoginWrap>
+      <LoginContainer>
+        <img src={logoImg} alt="" />
+        <article></article>
+        <nav>
+          <Link to="/">회원가입</Link>
+          <Link to="/">비밀먼호 찾기</Link>
+        </nav>
+      </LoginContainer>
+    </LoginWrap>
   );
 }
