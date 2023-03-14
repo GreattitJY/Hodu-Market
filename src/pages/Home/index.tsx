dbff07c2bc6a5fb83b4cf75aae0f0e04a7d1b6b2
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-14 11:47:37 +0900

    add: user 정보 props 전달

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Main from "../../components/Main";
+import { useLocation } from "react-router";
 
 export default function HomePage() {
+  const isBuyer = useLocation().state?.isBuyer;
+
   return (
     <>
-      <Header />
+      <Header {...{ isBuyer }} />
       <Main />
       <Footer />
     </>
