4d3fc0bc6bc180394ceeb02c2d4b45f2b0675f55
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-17 10:31:11 +0900

    add: 회원가입 라우터 설정

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -4,14 +4,19 @@ import NotFoundPage from "./404";
 import CartPage from "./Cart";
 import HomePage from "./Home";
 import LoginPage from "./Login";
+import SignupPage from "./Signup";
 
 export default function AppRouter() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+
         <Route path="/cart" element={<CartPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
         <Route path="*" element={<NotFoundPage />}></Route>
       </Routes>
     </BrowserRouter>
