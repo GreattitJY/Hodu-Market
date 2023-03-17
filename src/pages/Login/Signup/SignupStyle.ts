88526b74ffb397595f2e678006dc4a3161dc4d15
GreattitJY <great.tit.jaejae@gmail.com>
2023-03-17 11:25:52 +0900

    add : 회원가입 페이지 마크업 및 스타일 초안

---
+++ src/pages/Login/Signup/SignupStyle.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SignupFieldset = styled.fieldset`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    /* padding: 20px 0px;
+    border-bottom: 1px solid ${({ theme }) => theme.color.gray200}; */
+  }
+
+  button {
+    /* width: 480px;
+    height: 60px;
+    margin: 36px auto 0;
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.color.green100};
+    color: #fff; */
+  }
+`;
---
+++ src/pages/Login/Signup/index.tsx
@@ -0,0 +1,77 @@
+import React, { useRef, useState } from "react";
+import {
+  LoginContainer,
+  LoginForm,
+  LoginWrap,
+  LogoImg,
+  LogoLink,
+  UserSelectBtn,
+  UserSelectBtnContainer,
+} from "../LoginStyle";
+import { SignupFieldset } from "./SignupStyle";
+
+export default function Signup() {
+  const logoImg = "/assets/images/Logo-hodu.png";
+
+  const userBtnRef = useRef<HTMLButtonElement[] | null[]>([]);
+  const [isBuyer, setIsBuyer] = useState<boolean>(true);
+
+  const handleUserLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.target === userBtnRef.current[0]) {
+      setIsBuyer(true);
+    } else if (e.target === userBtnRef.current[1]) {
+      setIsBuyer(false);
+    }
+  };
+
+  return (
+    <LoginWrap>
+      <LoginContainer>
+        <LogoLink to="/">
+          <LogoImg src={logoImg} alt="" />
+        </LogoLink>
+        <UserSelectBtnContainer>
+          <UserSelectBtn onClick={handleUserLogin} ref={(buyer) => (userBtnRef.current[0] = buyer)} {...{ isBuyer }}>
+            구매회원 가입
+          </UserSelectBtn>
+          <UserSelectBtn onClick={handleUserLogin} ref={(seller) => (userBtnRef.current[1] = seller)} {...{ isBuyer }}>
+            판매회원 가입
+          </UserSelectBtn>
+        </UserSelectBtnContainer>
+        <LoginForm id="info">
+          <SignupFieldset>
+            <legend className="sr-out">회원 정보 입력</legend>
+
+            <label htmlFor="id">아이디</label>
+            <input type="text" name="id" />
+            <button>중복확인</button>
+
+            <label htmlFor="password">비밀번호</label>
+            <input type="text" name="password" />
+
+            <label htmlFor="passwordCheck">비밀번호 재확인</label>
+            <input type="text" name="passwordCheck" />
+
+            <label htmlFor="name">이름</label>
+            <input type="text" name="name" />
+
+            <label htmlFor="phone">휴대폰번호</label>
+            <input type="text" name="phone" />
+            <input type="text" name="phone" />
+            <input type="text" name="phone" />
+
+            <label htmlFor="mail">이메일</label>
+            <input type="text" name="mail" />
+            <span>@</span>
+            <input type="text" name="mail" />
+          </SignupFieldset>
+        </LoginForm>
+        <label>
+          <input type="checkbox" name="agree" />
+          호두샵의 이용약관 및 개인정보처리방침에 대한 내용을 확인하였고 동의합니다.
+        </label>
+        <button form="info">가입하기</button>
+      </LoginContainer>
+    </LoginWrap>
+  );
+}
